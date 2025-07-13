73d2cd9ab5df19afd30f8eb0b6b81a371d121de6
Leigh London <leigh.00.london@gmail.com>
2025-07-13 22:26:01 +1000

    add docs

--- requestid.go
+++ requestid.go
@@ -8,10 +8,13 @@ type IDKey struct{}
 
 const Header = "X-Request-Id"
 
+// IntoContext injects the provided requestid into the context.
 func IntoContext(ctx context.Context, id string) context.Context {
 	return context.WithValue(ctx, IDKey{}, id)
 }
 
+// FromContext extracts the requestid from the context or returns
+// the empty string.
 func FromContext(ctx context.Context) string {
 	if id, ok := ctx.Value(IDKey{}).(string); ok {
 		return id
